5826c1cac359ad9f6ca2464c94b803aad76ca0a0
guestSoftMe <73720335+guestSoftMe@users.noreply.github.com>
2020-11-17 23:02:34 +0200

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu_header').addEventListener('click',function () {
+    document.querySelector('.menu_header span').classList.toggle('active')
+    document.querySelector('.menu_menu_list').classList.toggle('hidden')
+})
